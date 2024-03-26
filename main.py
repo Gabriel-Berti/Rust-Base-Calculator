2ff95df79c8d408483e9653b99cc70aecd37674e
Gabriel Berti <gabrielberti0109@gmail.com>
2024-03-25 22:02:32 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from dotenv import dotenv_values
+from pymongo import MongoClient
+from routes import router as book_router
+
+config = dotenv_values(".env")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.mongodb_client = MongoClient(config["ATLAS_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    yield
+    app.mongodb_client.close()
+
+app = FastAPI(lifespan=lifespan)   
+
+app.include_router(book_router, tags=["books"], prefix="/book")
